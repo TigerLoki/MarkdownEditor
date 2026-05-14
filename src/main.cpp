309fdf3199825c59920de5af895d3d348aa256e4
Denis Gusarov <gusaaroff@gmail.com>
2026-05-15 01:58:31 +0200

    added icon

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    app.setWindowIcon(QIcon(":/app/icons/icon.png"));
 
     QQmlApplicationEngine engine;
     engine.loadFromModule("MarkdownEditor", "Main");
